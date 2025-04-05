4075d884ee6305e65e3bbe2e380e5cb882c2bcb3
Emi004 <aenoaeiemi49@gmail.com>
2025-04-05 17:21:40 +0300

    minor typo

--- frontend/src/Pages/AddItemsPage.jsx
+++ frontend/src/Pages/AddItemsPage.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState, useEffect, use } from "react";
+import { useState, useEffect } from "react";
 import "../css/AddItemsPage.css";
 import categories from "../assets/CategorySubcategory.jsx";
 
