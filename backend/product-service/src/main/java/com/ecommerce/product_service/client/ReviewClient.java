9223b7d2569dfcfae9bf75711dc406994db0bc39
Adelin <adelin10emil@gmail.com>
2025-04-08 11:19:03 +0300

    Add security configuration and update application properties for OAuth2 support

--- backend/product-service/src/main/java/com/ecommerce/product_service/client/ReviewClient.java
+++ backend/product-service/src/main/java/com/ecommerce/product_service/client/ReviewClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "review-service", url = "${REVIEW_SERVICE_URL:http://localhost:8084/api/review/}")
+@FeignClient(name = "review-service", url = "${REVIEW_SERVICE_URL:http://localhost:8000/api/review/}")
 public interface ReviewClient {
 
     @GetMapping("product-average/{productId}")
