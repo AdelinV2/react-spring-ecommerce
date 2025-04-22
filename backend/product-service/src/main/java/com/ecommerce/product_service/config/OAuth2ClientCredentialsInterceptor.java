2b0ab2dbd8b586da26f91e2b2091ba6b09e2dd8a
Adelin <adelin10emil@gmail.com>
2025-04-22 09:07:45 +0300

    Update docker-compose.yml and OAuth2ClientCredentialsInterceptor: add Keycloak configuration and adjust Kafka listeners

--- backend/product-service/src/main/java/com/ecommerce/product_service/config/OAuth2ClientCredentialsInterceptor.java
+++ backend/product-service/src/main/java/com/ecommerce/product_service/config/OAuth2ClientCredentialsInterceptor.java
@@ -2,6 +2,7 @@ package com.ecommerce.product_service.config;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
@@ -9,6 +10,7 @@ import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.stereotype.Component;
 
 @Component
+@DependsOn("authorizedClientManager")
 public class OAuth2ClientCredentialsInterceptor implements RequestInterceptor {
 
     private final OAuth2AuthorizedClientManager authorizedClientManager;
