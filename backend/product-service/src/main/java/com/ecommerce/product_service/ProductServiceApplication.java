308145c542bee52a5a3b621fc15375968703a1c5
Adelin <adelin10emil@gmail.com>
2025-04-22 15:02:50 +0300

    Import OAuth2ClientConfig in ProductServiceApplication to enable OAuth2 client configuration

--- backend/product-service/src/main/java/com/ecommerce/product_service/ProductServiceApplication.java
+++ backend/product-service/src/main/java/com/ecommerce/product_service/ProductServiceApplication.java
@@ -1,11 +1,14 @@
 package com.ecommerce.product_service;
 
+import com.ecommerce.common.config.OAuth2ClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
 
 @EnableFeignClients(basePackages = "com.ecommerce.product_service.client")
 @SpringBootApplication
+@Import(OAuth2ClientConfig.class)
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
